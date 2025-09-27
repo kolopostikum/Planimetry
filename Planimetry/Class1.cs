ad5f188343d92293b76c463fecc437a294c1b9dd
said <kerorogunso@mail.ru>
2025-09-27 15:46:07 +0300

    Add project files.

---
+++ Planimetry/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Planimetry
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ PlanimetryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PlanimetryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

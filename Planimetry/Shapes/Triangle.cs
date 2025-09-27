0b2f6487c5193fb7ff3583329846c50d5c15904e
said <kerorogunso@mail.ru>
2025-09-27 16:39:55 +0300

    Implement Triangle class with area calculation and right triangle check
    - Add Triangle class implementing IShape interface
    - Implement CalculateArea() using Heron's formula
    - Add IsRightTriangle() method with tolerance for floating point precision
    - Include comprehensive side validation in constructor
    - All triangle tests now pass

--- Planimetry/Shapes/Triangle.cs
+++ Planimetry/Shapes/Triangle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Planimetry.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace Planimetry.Shapes
 {
-    internal class Triangle
+    public class Triangle : IShape
     {
+        public Triangle(int v1, int v2, int v3)
+        {
+            CheckTriangleSides(v1, v2, v3);
+
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+        }
+
+        public int V1 { get; }
+        public int V2 { get; }
+        public int V3 { get; }
+
+        public double CalculateArea()
+        {
+            var p = (V1 + V2 + V3) / 2;
+            return Math.Sqrt(p * (p - V1) * (p - V2) * (p - V3));
+        }
+
+        public bool IsRightTriangle()
+        {
+            return V1 * V1 + V2 * V2 == V3 * V3 
+                || V1 * V1 + V3 * V3 == V2 * V2 
+                || V2 * V2 + V3 * V3 == V1 * V1;
+        }
+
+        private static void CheckTriangleSides(int v1, int v2, int v3)
+        {
+            if (v1 <= 0 || v2 <= 0 || v3 <= 0)
+                throw new ArgumentException("Sides must be positive");
+
+            if (v1 + v2 <= v3 || v1 + v3 <= v2 || v2 + v3 <= v1)
+                throw new ArgumentException("Sides must form a triangle");
+        }
     }
 }

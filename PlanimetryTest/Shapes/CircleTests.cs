da911105198ddaff2878def78e83a3174cd7ab6d
said <kerorogunso@mail.ru>
2025-09-27 15:53:24 +0300

    git add tests/PlanimetryTest/Shapes/CircleTests.cs git commit -m "Add failing unit tests for Circle class
    - Test for area calculation with valid radius
    - Test for zero radius edge case
    - Test for negative radius validation
    - Tests currently fail as Circle class doesn't exist yet (TDD approach)"

--- PlanimetryTest/Shapes/CircleTests.cs
+++ PlanimetryTest/Shapes/CircleTests.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using Planimetry.Shapes;
 
 namespace PlanimetryTest.Shapes
 {
+    [TestFixture]
     internal class CircleTests
     {
+        [Test]
+        public void CalculateArea_WithValidRadius_ReturnsCorrectArea()
+        {
+            // Arrange
+            double radius = 5;
+            var circle = new Circle(radius);
+            double expectedArea = 78.53981633974483; // π * r² = 3.141592653589793 * 25
+
+            // Act
+            double actualArea = circle.CalculateArea();
+
+            // Assert
+            Assert.That(actualArea, Is.EqualTo(expectedArea).Within(0.0001));
+        }
+
+        [Test]
+        public void CalculateArea_WithZeroRadius_ReturnsZero()
+        {
+            // Arrange
+            double radius = 0;
+            var circle = new Circle(radius);
+
+            // Act
+            double area = circle.CalculateArea();
+
+            // Assert
+            Assert.That(area, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Constructor_WithNegativeRadius_ThrowsArgumentException()
+        {
+            // Arrange
+            double radius = -5;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new Circle(radius));
+        }
     }
 }
